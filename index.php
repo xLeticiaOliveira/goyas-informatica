6f1fc75e38e2e792aa0e4496db2a545e3a497e0b
Letícia Oliveira <leticia164_@hotmail.com>
2021-03-03 15:21:27 -0300

    Adição de divs

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <h1> Goyas Informática </h1>
 <p> Assistência Técnica em computadores e notebooks.</p>
 
+<div style="width: 100%;display:flex">
+    <div style="width: 50%;background: blue"></div>
+    <div style="width: 50%;background: red"></div>
+</div>
+
 </body>
 </html>
