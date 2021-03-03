52ccecadc8eede96f98c4d35ec120e253a77e704
Letícia Oliveira <leticia164_@hotmail.com>
2021-03-03 15:02:50 -0300

    Retirada da papelaria

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>Goyas Informática</title>
 </head>
 <body>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+<h1> Goyas Informática </h1>
+<p> Assistência Técnica em computadores e notebooks.</p>
 
 </body>
 </html>
